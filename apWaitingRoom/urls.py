9f5fcfe1b34e5aeec7c492e174088ebd69f9896d
BrendanHurt <brendan.q.hurt@gmail.com>
2026-01-08 10:27:27 -0800

    Lint & changed admin url to prevent confusion

--- apWaitingRoom/urls.py
+++ apWaitingRoom/urls.py
@@ -18,6 +18,6 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path('profile/', include("users.urls")),
-    path('host/', admin.site.urls),
+    path('profile/', include('users.urls')),
+    path('adminpelago/', admin.site.urls),
 ]
