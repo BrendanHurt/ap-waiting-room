c38c21d358f1d0022520565512bde647b0a603da
BrendanHurt <brendan.q.hurt@gmail.com>
2026-01-19 13:55:24 -0800

    More detailed users __str__ method

--- users/models.py
+++ users/models.py
@@ -5,3 +5,6 @@ class users(models.Model):
     name = models.CharField(max_length = 32)
     email = models.CharField(max_length = 200)
     status = models.CharField(max_length = 32)
+
+    def __str__(self):
+        return f"{self.name}, {self.email}, {self.status}"
