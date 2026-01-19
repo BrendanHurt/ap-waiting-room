61a96c2dfda276de3ac8e6430fea240fc97ceb80
BrendanHurt <brendan.q.hurt@gmail.com>
2026-01-19 14:20:28 -0800

    Added basic account page for users

--- users/urls.py
+++ users/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.index, name="index")
+    path('', views.index, name='index'),
+    path('<int:user_id>/', views.account, name='account'),
 ]
--- users/views.py
+++ users/views.py
@@ -1,5 +1,16 @@
 from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404
+
+from .models import users
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Hello world, you are at the users index")
+    return HttpResponse('This is where the login page will go')
+
+def account(request, user_id):
+    user = get_object_or_404(users, pk=user_id)
+    return render(
+        request,
+        'users/account.html',
+        {"user": user}
+    )
