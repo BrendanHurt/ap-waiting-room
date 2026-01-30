5c969dfc3ccec7178b721526b83907aa50613395
BrendanHurt <brendan.q.hurt@gmail.com>
2026-01-30 10:30:44 -0800

    Changed schema for storing YAMLs
    Instead of storing the actual files, it will store the text from them, and write that text to a file when downloaded.

--- user_yamls/models.py
+++ user_yamls/models.py
@@ -4,11 +4,11 @@ from users.models import users
 # Create your models here.
 class user_yamls(models.Model):
     user_id = models.ForeignKey(users, on_delete=models.CASCADE)
-    yaml_file = models.FileField()
     slot = models.CharField(max_length=16, default="Default Slot")
     game_name = models.CharField(max_length=50, default="Default Game Name")
     description = models.CharField(max_length=1000, null=True)
+    game_options = models.CharField(default="EMPTY SETTINGS")
     last_edited = models.DateField(auto_now = True)
 
     def __str__(self):
-        return 'yup'
+        return f"{self.slot}\n{self.game_name}\n{self.description}\n\n{self.game_options}"
