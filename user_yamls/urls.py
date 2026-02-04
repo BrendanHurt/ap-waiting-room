ecef1bf42184cb9f5dfa13d9ef13d8732ecfa875
BrendanHurt <brendan.q.hurt@gmail.com>
2026-02-03 18:09:55 -0800

    Basic form of users adding yamls
    There currently isn't any validation of the yaml data. It's just taking in the fields as strings.

--- user_yamls/urls.py
+++ user_yamls/urls.py
@@ -5,5 +5,6 @@ from . import views
 app_name = "user_yamls"
 urlpatterns = [
     path('<int:user_id>/', views.index, name='view_yamls'),
-    path('add/', views.add, name='add_yaml'),
+    path('yaml/', views.yaml_form, name='yaml_form'),
+    path('yaml/save/', views.add, name='add_yaml'),
 ]
--- user_yamls/views.py
+++ user_yamls/views.py
@@ -1,7 +1,10 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render, get_object_or_404
+from django.db import DatabaseError
+from django.urls import reverse
 
 from users.models import users
+from .models import user_yamls
 
 # Create your views here.
 def index(request, user_id):
@@ -12,5 +15,34 @@ def index(request, user_id):
     )
     #return HttpResponse('This is the index page for yamls')
 
+def yaml_form(request):
+    return render(request, 'user_yamls/add_yaml.html')
+
 def add(request):
-    return HttpResponse('You have reached the add a yaml page. Please leave your message after the beep... beep')
+    #get the user_id
+    #get the slot name
+    #get the game name
+    #get the description, emtpy string if none
+    #get the options
+    try:
+        user = get_object_or_404(
+            users,
+            pk=request.session.get("user_id")
+        )
+        new_yaml = user_yamls(
+            user_id = user,
+            slot = request.POST["slot"],
+            game_name = request.POST["game_name"],
+            description = request.POST["description"],
+            game_options = request.POST["game_options"]
+        )
+
+        new_yaml.save()
+        return HttpResponseRedirect(
+            reverse(
+                "user_yamls:view_yamls",
+                args=(user.id,)
+            )
+        )
+    except DatabaseError:
+        return HttpResponse(DatabaseError)
