576494d728afd9a0b62a2ab3282cf55640a3bdc6
BrendanHurt <brendan.q.hurt@gmail.com>
2026-03-02 10:17:01 -0800

    Added lobby joining and viewing lobbies
    Users can view a list of all lobbies with condensed info. They can then select a lobby's join button. Which brings them to a form for selecting YAMLs they've uploaded. The user can select at least one of the YAMLs, and then submit them. The YAMLs will be added to the lobby as "lobby connections" and the user is redirected to the lobby's detailed info page.

--- Lobby/urls.py
+++ Lobby/urls.py
@@ -8,4 +8,8 @@ urlpatterns = [
     path("new/", views.new_lobby_form, name="create_lobby"),
     path("insert/", views.insert_lobby, name="insert_lobby"),
     path("del/<int:lobby_id>", views.delete_lobby, name="delete_lobby"),
+
+    path("<int:lobby_id>", views.view_lobby, name="view_lobby"),
+    path("<int:lobby_id>/join/yamls", views.select_yamls, name="start_lobby_join"),
+    path("<int:lobby_id>/join", views.join_lobby, name="join_lobby"),
 ]
--- Lobby/views.py
+++ Lobby/views.py
@@ -2,9 +2,14 @@ from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse, HttpResponseRedirect
 from django.urls import reverse
 from django.utils.timezone import now
+from django.db import DatabaseError
 
 from .models import Lobby, LobbyConnection
 from users.models import users
+from user_yamls.models import user_yamls
+
+#----------------------------------------------
+# Lobby Views
 
 # Create your views here.
 def manage_lobbies(request):
@@ -52,5 +57,59 @@ def delete_lobby(request, lobby_id):
         reverse(
             "Lobby:manage_lobbies",
             args=()
+        )
+    )
+
+
+def view_lobby(request, lobby_id):
+    #get the lobby
+    #then, get any yamls the current user has sent to this lobby
+        #find yaml by user
+    #add those to the context & send to the lobby info template
+    lobby = get_object_or_404(Lobby, pk=lobby_id)
+    yamls = user_yamls.objects.filter(
+        lobbyconnection__lobby_id=lobby_id
+
+    )
+    
+    return HttpResponse(
+        render(
+            request, 
+            "Lobby/lobby_connections_info.html",
+            {"lobby": lobby, "yamls": yamls}
+        )
+    )
+
+#----------------------------------------------
+# Lobby Connection Views
+def select_yamls(request, lobby_id):
+    yaml_list = user_yamls.objects.filter(pk=request.session.get("user_id"))
+    request.session["lobby_id"] = lobby_id
+    return HttpResponse(
+        render(
+            request,
+            "Lobby/select_yaml.html",
+            {"yaml_list": yaml_list, "lobby_id": lobby_id}
+        ),
+    )
+
+def join_lobby(request, lobby_id):
+    lobby = get_object_or_404(Lobby, pk=lobby_id)
+    yaml_ids = request.POST["yaml_ids"]
+
+    for yaml_id in yaml_ids:
+        yaml = get_object_or_404(user_yamls, pk=yaml_id)
+        try:
+            l = LobbyConnection.objects.create(
+                lobby_id=lobby,
+                player_yaml=yaml
+            )
+        except DatabaseError:
+            messages.error(request, "Failed to send yaml %s" %yaml_id)
+
+    return HttpResponseRedirect(
+        reverse(
+            "Lobby:view_lobby",
+            kwargs={"lobby_id": lobby_id}
         )
     )
