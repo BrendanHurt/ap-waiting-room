f08255222aba1895f2c8cee207de229afe6665e6
BrendanHurt <brendan.q.hurt@gmail.com>
2026-03-23 10:29:22 -0700

    Revert "Removed unnecessary view"
    This reverts commit 06a68f6c2a3999799a18aa0768e36533ddcb427c. Missed removing the url to the view.

--- users/views.py
+++ users/views.py
@@ -6,6 +6,10 @@ from django.contrib import messages
 
 from .models import users
 
+# Create your views here.
+def index(request):
+    return HttpResponse('This is where the login page will go')
+
 def account(request, user_id):
     try:
         user = users.objects.get(pk=request.session["user_id"])
