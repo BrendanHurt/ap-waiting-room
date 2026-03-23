ee0c8db2c0d319f47904d3186ed90d2f451a6b7f
BrendanHurt <brendan.q.hurt@gmail.com>
2026-03-23 12:57:03 -0700

    Removed unnecessary view

--- users/urls.py
+++ users/urls.py
@@ -4,7 +4,6 @@ from . import views
 
 app_name = 'users'
 urlpatterns = [
-    path('', views.index, name='index'),
     path('auth/', views.user_auth, name="user_auth"),
     path('<int:user_id>/account/', views.account, name='account'),
     path('logout/', views.logout, name="logout")
